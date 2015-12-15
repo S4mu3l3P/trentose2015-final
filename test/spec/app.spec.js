3dc25788185ecd4a525f331f97cb8aeea78893c9
S4mu3l3P <samuelepinginf@gmail.com>
2015-12-15 17:27:38 +0100

    first commit

--- test/spec/app.spec.js
+++ test/spec/app.spec.js
@@ -3,8 +3,22 @@
 
 describe("Santa", function() {
 
-  it("should...", function() {  
+   it("Return the correct current request", function () {
+       SantaModel.init();
+       expect(SantaModel.getCurrentRequest()).toBe(requests[0]);
+       SantaModel.next();
+       expect(SantaModel.getCurrentRequest()).toBe(requests[1]);
+       SantaModel.next();
+       SantaModel.next();
+       SantaModel.next();
+       expect(SantaModel.getCurrentRequest()).toBe(null);
+    });   
     
-  });    
+    it("Correct packing", function () {
+        expect(SantaModel.pack("yes")).toBe(0);
+        expect(SantaModel.pack("no")).toBe(1);
+        SantaModel.next();
+        expect(SantaModel.pack("yes")).toBe(1);
+    }); 
  
 });
